aa588aff5fad335f69d87fe2070c2443bc4b48e9
MktR0 <mktronr@gmail.com>
2024-12-10 20:57:48 -0500

    initial commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode, TextType 
+
+def main():
+    sample_text_node = TextNode("This is a test of the text node system", TextType.BOLD, "helloWorld.com")
+    print(sample_text_node)
+
+
+
+main()
+
+

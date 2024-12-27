d03e921532d74999d140a2c1c48f0037d5a117cd
MktR0 <mktronr@gmail.com>
2024-12-27 01:09:54 -0500

    Add LeafNode and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,7 +6,7 @@ class HTMLNode:
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError("This funcionality is not implemented yet.")
+        raise NotImplementedError("to_html is not implemented")
 
     def props_to_html(self):
         if self.props == None:
@@ -19,4 +19,25 @@ class HTMLNode:
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+
+    def __setattr__(self, name, value):
+        if name == "children" and hasattr(self, "children"):
+            raise AttributeError("Leafnode cannot have children")
+        super().__setattr__(name, value)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("All leaf nodes must have a value")
+        if not self.tag:
+            return f"{self.value}"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,16 +1,14 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
         node = HTMLNode(tag="a", value=None, children=None, props=None)
-
         self.assertEqual("HTMLNode(a, None, None, None)", repr(node))
 
     def test_no_props(self):
         node = HTMLNode(tag="h1", value="Welcome", children=None, props=None)
-
         self.assertEqual(node.props_to_html(), "")
 
     def test_single_prop(self):
@@ -40,3 +38,36 @@ class TestHTMLNode(unittest.TestCase):
             node.props_to_html(),
             ' class="main-content" id="section1" style="color: red; font-size: 14px" data-custom="value"',
         )
+        
+
+    def test_none_value_raise_error(self):
+        node = LeafNode(tag="p",value=None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_children_starts_none(self):
+        node = LeafNode(tag="p", value="This is text")
+        self.assertIsNone(node.children)
+
+    def test_cannot_add_children(self):
+        node = LeafNode(tag="p", value="This is text")
+        with self.assertRaises(AttributeError):
+            node.children = [LeafNode("a", "unacceptable text")]
+
+
+    def test_only_value(self):
+        node = LeafNode(tag="p",value="This is a paragraph of text")
+        self.assertEqual(node.to_html(),"<p>This is a paragraph of text</p>")
+
+    def test_one_leaf(self):
+        node = LeafNode(tag="a", value="Click me!", props={"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(),'<a href="https://www.google.com">Click me!</a>')
+
+    def test_no_tag(self):
+        node = LeafNode(None, "Hello, Buddy!")
+        self.assertEqual(node.to_html(), "Hello, Buddy!")
+
+
+if __name__ == "__main__":
+    unittest.main()
+

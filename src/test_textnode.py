b749626bb93a644468c8f92679455e29717a3193
MktR0 <mktronr@gmail.com>
2025-01-02 23:55:27 -0500

    Add text_node_to_html_node function with full tests and error handling

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,8 @@
+from typing import Text
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -19,7 +21,6 @@ class TestTextNode(unittest.TestCase):
         self.assertIsNone(node.url)
         self.assertIsNone(node2.url)
 
-
     def test_not_eq_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
         node2 = TextNode("A different text node", TextType.TEXT)
@@ -27,7 +28,7 @@ class TestTextNode(unittest.TestCase):
 
     def test_not_eq_url(self):
         node = TextNode("This is a text node", TextType.BOLD, url=None)
-        node2 = TextNode("This is a text node", TextType.BOLD,url="")
+        node2 = TextNode("This is a text node", TextType.BOLD, url="")
         self.assertNotEqual(node, node2)
 
     def test_not_eq_texttype(self):
@@ -36,11 +37,97 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node2)
 
     def test_repr(self):
-        node = TextNode("This is a text node", TextType.TEXT,"https://docs.python.org/3/howto/enum.html" )
+        node = TextNode(
+            "This is a text node",
+            TextType.TEXT,
+            "https://docs.python.org/3/howto/enum.html",
+        )
         self.assertEqual(
-            "TextNode(This is a text node, text, https://docs.python.org/3/howto/enum.html)", repr(node)
-            )
+            "TextNode(This is a text node, text, https://docs.python.org/3/howto/enum.html)",
+            repr(node),
+        )
 
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_all_types(self):
+        for text_type in TextType:
+            if text_type == TextType.TEXT:
+                node = TextNode(text="Sample", text_type=text_type)
+                html_node = text_node_to_html_node(node)
+                self.assertIsNone(html_node.tag)
+                self.assertEqual(html_node.value, "Sample")
+
+            elif text_type == TextType.BOLD:
+                node = TextNode("This is bold", TextType.BOLD)
+                html_node = text_node_to_html_node(node)
+                self.assertEqual(html_node.tag, "b")
+                self.assertEqual(html_node.value, "This is bold")
+
+            elif text_type == TextType.ITALIC:
+                node = TextNode("This is italic", TextType.ITALIC)
+                html_node = text_node_to_html_node(node)
+                self.assertEqual(html_node.tag, "i")
+                self.assertEqual(html_node.value, "This is italic")
+
+            elif text_type == TextType.CODE:
+                node = TextNode("This is code", TextType.CODE)
+                html_node = text_node_to_html_node(node)
+                self.assertEqual(html_node.tag, "code")
+                self.assertEqual(html_node.value, "This is code")
+
+            elif text_type == TextType.LINK:
+                node = TextNode(
+                    "This is a link",
+                    TextType.LINK,
+                    url="https://en.ac-illust.com/clip-art/24690570/free-cut-picture-material-cute-penguin",
+                )
+                html_node = text_node_to_html_node(node)
+                self.assertEqual(html_node.tag, "a")
+                self.assertEqual(html_node.value, "This is a link")
+                self.assertEqual(
+                    html_node.props,
+                    {
+                        "href": "https://en.ac-illust.com/clip-art/24690570/free-cut-picture-material-cute-penguin"
+                    },
+                )
+
+            elif text_type == TextType.IMAGE:
+                node = TextNode(
+                    "This is an image",
+                    TextType.IMAGE,
+                    url="https://en.ac-illust.com/clip-art/24690570/free-cut-picture-material-cute-penguin",
+                )
+                html_node = text_node_to_html_node(node)
+                self.assertEqual(html_node.tag, "img")
+                self.assertEqual(html_node.value, "")
+                self.assertEqual(
+                    html_node.props,
+                    {
+                        "src": "https://en.ac-illust.com/clip-art/24690570/free-cut-picture-material-cute-penguin",
+                        "alt": "This is an image",
+                    },
+                )
+
+    def test_missing_url_image(self):
+        node = TextNode(text="missing url test", text_type=TextType.IMAGE, url="")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
+
+    def test_missing_url_link(self):
+        node = TextNode(text="Missing url test", text_type=TextType.LINK)
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
+
+    def test_missing_alt_image(self):
+        node = TextNode(text="", text_type=TextType.IMAGE)
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
+
+    def test_invalid_text_type(self):
+        # Assumption "InvalidType" is not part of the TextType enum
+        node = TextNode(text="Invalid type test", text_type="InvalidType")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from htmlnode import LeafNode
+
 
 class TextType(Enum):
 
@@ -6,21 +8,48 @@ class TextType(Enum):
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
-    LINKS = "links"
-    IMAGES = "images"
+    LINK = "link"
+    IMAGE = "image"
+
 
 class TextNode:
-    def __init__(self,text, text_type, url=None):
-        self.text = text 
-        self.text_type = text_type 
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
         return (
-                self.text_type == other.text_type 
-                and self.text == other.text 
-                and self.url == other.url
-                )
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+
+def text_node_to_html_node(text_node):
+
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b",text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i",text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code",text_node.text)
+    if text_node.text_type == TextType.LINK:
+        if not text_node.url:
+            raise ValueError("Node requires valid url")
+        return LeafNode("a",text_node.text,props={"href":text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        if not text_node.url:
+            raise ValueError("Node requires valid url")
+        if not text_node.text:
+            raise ValueError("Node requires alt text")
+        return LeafNode("img","",props={"src": text_node.url, "alt":text_node.text})
+    raise ValueError("Invalid text type")
+
+
